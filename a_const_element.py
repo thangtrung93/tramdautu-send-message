2732123d6ed195f93aff5a6b8871387035698f2d
thangtrung93 <thangtrung93@gmail.com>
2024-06-04 21:49:39 +0700

    update main

--- a_const_element.py
+++ a_const_element.py
@@ -2,11 +2,15 @@ api_id= "22426383"
 api_hash = "dbe56731ecfba79f74871b0b486556b1"
 bot_token = "7003690986:AAGDhUUKlEUPJjwcD7_JYHgqHbZ9Fs-ZclI"
 
+# Điền username của Channel
 channel = "chungkhoanDNSE"
 
+# Điền địa chỉ lưu file danh sách thành viên
 file_path = "C:/Users/7400/Desktop/"
 file_name = f"{channel}.xlsx"
 
+# Nội dung tin nhắn
 content_message = '<a href=https://www.youtube.com/watch?v=Fv_vLklcldg> youtube </a> \n '
 
+# Danh sách người nhận
 l_target_user = ["@thangkhong"]
